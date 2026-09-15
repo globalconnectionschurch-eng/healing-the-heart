0bcb2b2baad8ee40356a7f2bb6571187b8dbadba
globalconnectionschurch-eng <globalconnectionschurch@gmail.com>
2026-09-15 11:59:46 -0600

    Add Moneris payment tracking fields

---
+++ migrations/0003_moneris_payment_fields.sql
@@ -0,0 +1,5 @@
+ALTER TABLE registrations ADD COLUMN payment_order_no TEXT;
+ALTER TABLE registrations ADD COLUMN payment_ticket TEXT;
+ALTER TABLE registrations ADD COLUMN payment_transaction_id TEXT;
+ALTER TABLE registrations ADD COLUMN paid_at TEXT;
+ALTER TABLE registrations ADD COLUMN payment_error TEXT;
